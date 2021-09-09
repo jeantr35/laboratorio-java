0487db0e2d36fcc601575276b71a4f5f2dfda4d8
jeantr35 <jean.trujillo.35@gmail.com>
2021-09-09 14:55:31 -0500

    Punto 15 funcional y completado

--- src/main/java/com/sofka/laboratorio/Puntos.java
+++ src/main/java/com/sofka/laboratorio/Puntos.java
@@ -217,4 +217,45 @@ public class Puntos {
 
     }
 
+    public void puntoQuince(){
+        String menu;
+        String seleccion;
+        menu = "    ****** GESTION CINEMATOGRÁFICA ********\n" +
+                "    1-NUEVO ACTOR\n" +
+                "    2-BUSCAR ACTOR\n" +
+                "    3-ELIMINAR ACTOR\n" +
+                "    4-MODIFICAR ACTOR\n" +
+                "    5-VER TODOS LOS ACTORES\n" +
+                "    6-VER PELICULAS DE LOS ACTORES\n" +
+                "    7-VER CATEGORIA DE LAS PELICULAS DE LOS ACTORES\n" +
+                "    8-SALIR\n" +
+                "    Por favor digite una opcion: ";
+
+        do {
+            System.out.print(menu);
+            seleccion = lectorTeclado.leerFrase();
+            switch (seleccion){
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
+                case "6":
+                    break;
+                case "7":
+                    break;
+                case "8":
+                    break;
+                default:
+                    System.out.println("OPCION INCORRECTA");
+            }
+        }
+        while (!seleccion.equalsIgnoreCase("8"));
+    }
+
 }
